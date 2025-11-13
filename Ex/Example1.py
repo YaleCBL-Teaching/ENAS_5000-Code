42e29451e306d2474004ace13b46274637484dd2
shervin issakhani <sh.issakhani1999@gmail.com>
2025-11-13 06:27:39 -0500

    update

--- Ex/Example1.py
+++ Ex/Example1.py
@@ -11,13 +11,20 @@ car = "Car"
 # Lists
 my_list = [1, 3.14, "Car"]
 
-# Arithmetic examples
-print(a + b)     # 5
-print(a * b)     # 6
-print(7 / 3)     # 2.333...
-print(7 % 3)     # 1
-print(2 ** 0.5)  # 1.414...
+# Arithmetic examples(Print them)
+# add a & b
+
+# multiply a & b
+
+# multiply a & b
+
+#divide b/a
+
+#remainder b to a
+
+#exponentiation a to b
 
 # Store result in a variable
 my_result = 10 * 2
 print(my_result)   # 20
+
